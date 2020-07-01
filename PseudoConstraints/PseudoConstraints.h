15f15513a8fea476457a68fb71801aaf71d13a5b
Zain <zain@digitify.com>
2020-07-01 13:42:10 +0500

    PseudoConstraints code

---
+++ PseudoConstraints/PseudoConstraints.h
@@ -0,0 +1,19 @@
+//
+//  PseudoConstraints.h
+//  PseudoConstraints
+//
+//  Created by Zain on 01/07/2020.
+//  Copyright © 2020 PseudoLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PseudoConstraints.
+FOUNDATION_EXPORT double PseudoConstraintsVersionNumber;
+
+//! Project version string for PseudoConstraints.
+FOUNDATION_EXPORT const unsigned char PseudoConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PseudoConstraints/PublicHeader.h>
+
+
